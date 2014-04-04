af9f725615856b082257a4e083cda7391db6db63
Jimmy Cao <jcao@gopivotal.com>
2014-04-04 14:59:26 +0800

    add jeckins version in it

---
+++ mtt/mtt_jeckins/lib/MTT/MPI/Install/Analyze/OMPI.pm
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2005-2006 The Trustees of Indiana University.
+#                         All rights reserved.
+# Copyright (c) 2006-2008 Cisco Systems, Inc.  All rights reserved.
+# Copyright (c) 2007      Sun Microsystems, Inc.  All rights reserved.
+# $COPYRIGHT$
+# 
+# Additional copyrights may follow
+# 
+# $HEADER$
+#
+
+package MTT::MPI::Install::Analyze::OMPI;
+
+use strict;
+use Data::Dumper;
+use MTT::DoCommand;
+use MTT::Messages;
+use MTT::FindProgram;
+use MTT::Values;
+use MTT::Files;
+use MTT::Values::Functions::MPI::OMPI;
+
+#--------------------------------------------------------------------------
+
+sub Install {
+    my ($ini, $section, $config) = @_;
+    my $x;
+    my $result_stdout;
+    my $result_stderr;
+
+    # Prepare $ret
+
+    my $ret;
+    $ret->{test_result} = MTT::Values::PASS;
+    $ret->{exit_status} = 0;
+
+    # Grab installdir parameter
+
+    $ret->{installdir} = $config->{module_data}->{installdir};
+    $ret->{bindir} = "$ret->{installdir}/bin";
+    $ret->{libdir} = "$ret->{installdir}/lib";
+
+    # Set which bindings were compiled
+
+    $ret->{c_bindings} = 1;
+    Debug("Have C bindings: 1\n");
+    my $func = \&MTT::Values::Functions::MPI::OMPI::find_bindings;
+    $ret->{cxx_bindings} = &{$func}($ret->{bindir}, $ret->{libdir}, "cxx");
+    Debug("Have C++ bindings: $ret->{cxx_bindings}\n"); 
+    $ret->{f77_bindings} = &{$func}($ret->{bindir}, $ret->{libdir}, "f77");
+    Debug("Have F77 bindings: $ret->{f77_bindings}\n"); 
+    $ret->{f90_bindings} = &{$func}($ret->{bindir}, $ret->{libdir}, "f90");
+    Debug("Have F90 bindings: $ret->{f90_bindings}\n"); 
+
+    return $ret;
+}
+
+1;
